86a9587f5ff03cba7a0c4d776bf78f6f69ac0a4c
Glitch (innate-fir-magnosaurus) <none>
2023-03-01 22:06:00 +0000

    Import of hmenni2/Discord_bot

---
+++ bot.py
@@ -0,0 +1,36 @@
+'''
+Created on Mar 1, 2023
+
+@author: Haskell
+'''
+# bot.py
+
+import os
+import discord
+from datetime import datetime, time
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+MEMBER = client.get_user(238093622568812544)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    printf(f'{client.user} has connected to {GUILD}.')
+    
+@client.listen()
+async def on_voice_state_update(MEMBER, before: discord.VoiceState, after: discord.VoiceState) -> None:
+    check_time = datetime.uutcnow().time
+    
+    if checktime <= time(22,59):
+        if after.channel is None:
+            return
+        else:
+            await MEMBER.edit(voice_channel=None)
+    else:
+        return
+    
+client.run(TOKEN)
