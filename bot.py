357c7a888dce70259b5ffd257d44e309bfb7c4b8
Glitch (innate-fir-magnosaurus) <none>
2023-03-02 18:31:53 +0000

    🍱🏙 Checkpoint ./bot.py:55309239/109

--- bot.py
+++ bot.py
@@ -18,11 +18,11 @@ async def on_ready():
     print(f'Logged in as {client.user}.')
     
 @client.event
-async def on_voice_state_update(MEMBER, before: discord.VoiceState, after: discord.VoiceState) -> None:
+async def on_voice_state_update(MEMBER, before: discord.VoiceState, after: discord.VoiceState):
     check_time = datetime.utcnow().time
     dotw = datetime.now().weekday()
     
-    if dotw < 5 and check_time < time(21,59):
+    if dotw < 5 and check_time <= time(21,59):
         if after.channel is None:
             return
         else:
